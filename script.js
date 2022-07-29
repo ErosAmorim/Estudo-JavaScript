d6a268b7d241b192a7f69b6db5fd2a41cb49aa8f
Eros Amorim <erosfsilva@gmail.com>
2022-07-28 23:44:54 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,51 @@
+/*let idade = 15;
+console.log(idade)
+let altura = '1,80cm';
+console.log(altura)
+let nome = 'Eros';
+console.log('nome')
+
+let pessoa = {
+	nome: 'Eros',
+	idade: '20',
+	estaAprovado: true,
+	sobrenome: 'Amorim'
+};
+
+let a = 5;
+let b = 5;
+let soma = a + b ;
+console.log(soma); */
+
+//Arrays
+
+/*let colegasNome = ['Lucas', 22, 'Alan', 21 , 'Dj', 'undefined'];
+console.log(colegasNome.length);
+console.log(colegasNome[5])*/
+
+//função
+/*let corSite = "azul"
+
+function resetaCor(cor, tonalidade){
+	corSite = cor + tonalidade;
+}; 
+
+console.log(corSite)
+resetaCor("Vermelho," " Escuro");
+console.log(corSite);
+
+let sorvete = " chocolate ";
+function sorveteSabor(sabor, cobertura){
+	sorvete = sabor + ' ' + cobertura;
+};
+
+console.log(sorvete);
+sorveteSabor("morango", "caramelo");
+console.log(sorvete);
+incremento e decremento
+let idade = 18
+
+console.log(--idade);
+console.log(++idade);
+*/
+
